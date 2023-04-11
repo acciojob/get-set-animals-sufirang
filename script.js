71da7769b7a37a2f5461ea2b12494759ecb5a3a9
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-11 16:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,75 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	constructor(species) {
+//     this._species = species;
+//   }
+
+//   get species() {
+//     return this._species;
+//   }
+
+//   makeSound() {
+//     console.log("The " + this._species + " makes a sound.");
+//   }
+// }
+
+// class Dog extends Animal {
+// 	bark() {
+//     console.log("woof");
+//   }
+// }
+
+// class Cat extends Animal {
+// 	 purr() {
+//     console.log("purr");
+//   }
+// }
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
 class Animal {
-	constructor(species) {
+  constructor(species) {
     this._species = species;
   }
-
   get species() {
     return this._species;
   }
-
   makeSound() {
-    console.log("The " + this._species + " makes a sound.");
+    console.log("The " + this.species + " makes a sound");
   }
 }
-
-class Dog extends Animal {
-	bark() {
-    console.log("woof");
-  }
-}
-
 class Cat extends Animal {
-	 purr() {
+  purr() {
     console.log("purr");
   }
 }
-
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
+
+
+// https://github.com/acciojob/get-set-animals-sufirang
